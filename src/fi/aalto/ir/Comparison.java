f2eaaeae312f88c92ad4b22bc68a86b71a7de2be
Patrik Vilja <patrik.vilja@gmail.com>
2014-04-14 08:56:03 +0300

    some testing and d3 graphs

--- src/fi/aalto/ir/Comparison.java
+++ src/fi/aalto/ir/Comparison.java
@@ -1,6 +1,7 @@
 package fi.aalto.ir;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -33,39 +34,61 @@ import org.apache.lucene.util.Version;
 
 public class Comparison {
 
-	public static Directory directory = null;
+	//public static Directory directory = null;
 	public Analyzer analyzer = null;
 
 	public Comparison() {
 
 	}
 
-	public void index(List<DocumentInCollection> docs, Similarity similarity) {
+    public void toCSV() {
+        File dataFile = new File(new File("curves"), "data.csv");
+        try {
+            FileWriter ostream = null;
+            try {
+                ostream = new FileWriter(dataFile);
+                // Write header line
+                ostream.write("recall, percision\n");
+                for (double i = 0.0; i <= 1.0; i++) {
+                    ostream.write(i + ", 0.35\n");
+                }
+                /*val dataMaps = dataSets.map(ds => ds.points.toMap)
+                val nValues = dataMaps.foldLeft(Set[XT]())((result, m) => result union m.keySet)
+                for (n <- nValues.toSeq.sorted) {
+                    ostream.write(n + ", " + dataMaps.map(m => if (m.contains(n)) m(n).toString else "").mkString(", ") + "\n")
+                }*/
+            } finally {
+                if (ostream != null) ostream.close();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+	public void index(List<DocumentInCollection> docs, Similarity similarity, Directory directory) {
 		analyzer = new StandardAnalyzer(Version.LUCENE_42); // Uses default
 															// StopAnalyzer.ENGLISH_STOP_WORDS_SET
 
-		IndexWriterConfig conf = new IndexWriterConfig(Version.LUCENE_42,
-				analyzer);
+		IndexWriterConfig conf = new IndexWriterConfig(Version.LUCENE_42, analyzer);
 		conf.setSimilarity(similarity);
 		conf.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
 		try {
 			IndexWriter writer = new IndexWriter(directory, conf);
-
 			for (DocumentInCollection doc : docs) {
 				/*
 				 * private String title; private String abstractText; private
 				 * int searchTaskNumber; private String query; private boolean
 				 * relevant;
 				 */
-				
+
 				if (doc.getSearchTaskNumber() == 16) {
 					String[] abs = doc.getAbstractText().split(" ");
 					String stemmed = "";
-					
+
 					for (String word : abs) {
 						stemmed += stem(word) + " ";
 					}
-					
+
 					Document document = new Document();
 					document.add(new Field("title", doc.getTitle(),
 							TextField.TYPE_STORED));
@@ -76,8 +99,8 @@ public class Comparison {
 					document.add(new Field("query", doc.getQuery(),
 							TextField.TYPE_STORED));
 					document.add(new Field("relevance", doc.isRelevant() ? "1" : "0",
-							TextField.TYPE_STORED));
-					
+                            TextField.TYPE_STORED));
+
 					writer.addDocument(document);
 				}
 			}
@@ -90,7 +113,7 @@ public class Comparison {
 
 	}
 
-	public List<String> search(List<String> inAbstract, Similarity similarity, String type) {
+	public List<String> search(List<String> inAbstract, Similarity similarity, String type, Directory directory) {
 
 		printQuery(inAbstract, type);
 
@@ -104,7 +127,7 @@ public class Comparison {
 
 			BooleanQuery booleanQuery = new BooleanQuery();
 			booleanQuery.setMinimumNumberShouldMatch(1);
-			
+
 			if (inAbstract != null) {
 				for (String term : inAbstract) {
 					Term termm = new Term("abstract", term);
@@ -150,7 +173,7 @@ public class Comparison {
 		} else
 			System.out.println(" no results");
 	}
-	
+
 	public static String stem(String word) {
 		Stemmer s = new Stemmer();
 		s.add(word.toCharArray(), word.length());
@@ -161,21 +184,25 @@ public class Comparison {
 	public static void main(String[] args) {
 		if (args.length > 0) {
 			String indexDir = System.getProperty("user.dir") + "/indicies";
+            String indexDir2 = System.getProperty("user.dir") + "/indicies2";
+            Directory directory = null;
+            Directory directory2 = null;
 
 			try {
 				directory = FSDirectory.open(new File(indexDir));
+                directory2  = FSDirectory.open(new File(indexDir2));
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			
+
 			String[] first_query = "Ergonomics and modern devices".split(" ");
 			String[] second_query = "ergonomics tablet typing".split(" ");
 			String[] third_query = "tablet and ergo*".split(" ");
-			
+
 			List<String> stemmed_first_query = new ArrayList<String>();
 			List<String> stemmed_second_query = new ArrayList<String>();
 			List<String> stemmed_third_query = new ArrayList<String>();
-			
+
 			for (String word : first_query) {
 				stemmed_first_query.add(stem(word));
 			}
@@ -195,39 +222,41 @@ public class Comparison {
 			parser.parse(args[0]);
 			List<DocumentInCollection> docs = parser.getDocuments();
 
-			comparisonVSM.index(docs, vsm);
-			comparisonBM25.index(docs, bm25);
-			
+			comparisonVSM.index(docs, vsm, directory);
+			comparisonBM25.index(docs, bm25, directory2);
 
 			List<String> inAbstract;
 			List<String> results;
-			
+            List<String> results2;
+
 			// 1) search document with word "tablet" in abstract and taskNumber 16 (BM25)
-			inAbstract = new LinkedList<String>();			
-			
+			inAbstract = new LinkedList<String>();
+
 			// First query
 			for (String word : stemmed_first_query) {
 				inAbstract.add(word);
 			}
-			results = comparisonBM25.search(inAbstract, bm25, "BM25");
+            results2 = comparisonVSM.search(inAbstract, vsm, "VSM", directory);
+            comparisonVSM.printResults(results2);
+
+			results = comparisonBM25.search(inAbstract, bm25, "BM25", directory2);
 			comparisonBM25.printResults(results);
-			
-			results = comparisonVSM.search(inAbstract, vsm, "VSM");
-			comparisonVSM.printResults(results);
-			
-			
+
+
+
+
 			// Second query
-			inAbstract = new LinkedList<String>();
+			/*inAbstract = new LinkedList<String>();
 			for (String word : stemmed_second_query) {
 				inAbstract.add(word);
 			}
 			results = comparisonBM25.search(inAbstract, bm25, "BM25");
 			comparisonBM25.printResults(results);
-			
+
 			results = comparisonVSM.search(inAbstract, vsm, "VSM");
 			comparisonVSM.printResults(results);
-			
-			
+
+
 			// Third query
 			inAbstract = new LinkedList<String>();
 			for (String word : stemmed_third_query) {
@@ -235,12 +264,13 @@ public class Comparison {
 			}
 			results = comparisonBM25.search(inAbstract, bm25, "BM25");
 			comparisonBM25.printResults(results);
-			
+
 			results = comparisonVSM.search(inAbstract, vsm, "VSM");
-			comparisonVSM.printResults(results);
+			comparisonVSM.printResults(results);*/
 
 			try {
-				directory.close();
+				if (directory != null) directory.close();
+                if (directory2 != null) directory2.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
